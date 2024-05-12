f7cc96b2864f0cbeaf2a5260a4b6271a944bbbf9
Christian Carmack <christian.carmack@outlook.com>
2024-05-12 18:18:35 +0000

    initial

---
+++ home.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "ccarmack";
+  home.homeDirectory = "/home/ccarmack";
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  home.packages = with pkgs; [
+    rustup
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/ccarmack/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "neovim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
